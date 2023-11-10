bfed09742e52212cf3f66b859fc773feab6c550c
Chris Boyd <christopherjzboyd@gmail.com>
2023-11-10 17:59:34 +0000

    First attempt at the script, copy item is failing, need to figure out how to get where-object to return the file or the path

--- Find Corrupted Files/MoveCorruptedFiles.ps1
+++ Find Corrupted Files/MoveCorruptedFiles.ps1
@@ -1,1 +1,11 @@
-Test-Certificate
+$TVPath = "E:\Media\TV"
+$MoviePath = "E:\Media\Movies"
+$CorruptedTV = Get-ChildItem -r -Path $TVPath | Where-Object Length -eq 0
+$CorruptedMovie = Get-ChildItem -r -Path $MoviePath | Where-Object Length -eq 0
+
+mkdir "E:\Media\Corrupted Files"
+mkdir "E:\Media\Corrupted Files\Corrupted Movies"
+mkdir "E:\Media\Corrupted Files\Corrupted TV"
+
+Copy-Item "E:\Media\TV\$CorruptedTV" "E:\Media\Corrupted Files\Corrupted TV" 
+Copy-Item "E:\Media\Movies\$CorruptedTV" "E:\Media\Corrupted Files\Corrupted Movies" 
