72e7a54c5c80ef0c405ce7c23aaad562f65b4ecc
Chris Boyd <christopherjzboyd@gmail.com>
2024-01-24 18:29:22 +0000

    up to date

--- Find Corrupted Files/MoveCorruptedFiles.ps1
+++ Find Corrupted Files/MoveCorruptedFiles.ps1
@@ -1,11 +1,11 @@
 $TVPath = "E:\Media\TV"
 $MoviePath = "E:\Media\Movies"
-$CorruptedTV = Get-ChildItem -r -Path $TVPath | Where-Object Length -eq 0 | Resolve-Path
-$CorruptedMovie = Get-ChildItem -r -Path $MoviePath | Where-Object Length -eq 0 | Resolve-Path
+$CorruptedTV = Get-ChildItem -r -Path $TVPath | Where-Object Length -eq 0 | Resolve-Path 
+$CorruptedMovie = Get-ChildItem -r -Path $MoviePath | Where-Object Length -eq 0 | Resolve-Path 
 
-mkdir "E:\Media\Corrupted Files"
-mkdir "E:\Media\Corrupted Files\Corrupted Movies"
-mkdir "E:\Media\Corrupted Files\Corrupted TV"
+#mkdir "E:\Media\Corrupted Files"
+#mkdir "E:\Media\Corrupted Files\Corrupted Movies"
+#mkdir "E:\Media\Corrupted Files\Corrupted TV"
 
 Move-Item -Path $CorruptedTV -Destination "E:\Media\Corrupted Files\Corrupted TV" 
 Move-Item -Path $CorruptedMovie -Destination "E:\Media\Corrupted Files\Corrupted Movies" 
